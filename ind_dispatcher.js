fae36a67a0847883556fcca968f5af830c77caf5
n.b.q.x <nbqxnbq@gmail.com>
2013-11-28 16:25:08 +0900

    use nconf

--- ind_dispatcher.js
+++ ind_dispatcher.js
@@ -1,4 +1,31 @@
-var app = require('./lib');
-var observe_folder = '../observed';
+var nconf = require('nconf'),
+    app = require('./lib');
 
-app.run(observe_folder);
+var conf = __dirname+'/conf.json';
+
+if(require('fs').existsSync(conf)){
+  nconf.file({file: conf});
+  var observe_folder = nconf.get('folder');
+  
+  if(observe_folder){
+    app.run(observe_folder);
+  }else{
+    console.log([
+    '\033[31m',
+    '[Exit]: need `folder` value',
+    '\033[39m'
+    ].join(''));
+    process.exit();
+  }
+  
+}else{
+
+  console.log([
+    '\033[31m',
+    '[Exit]: conf.json not found',
+    '\033[39m'
+  ].join(''));
+  process.exit();
+  
+}
+
