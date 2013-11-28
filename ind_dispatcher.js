55da57df00d87173608da8ed562c7c05e14eeb33
n.b.q.x <nbqxnbq@gmail.com>
2013-11-28 16:40:08 +0900

    conf.json++

--- ind_dispatcher.js
+++ ind_dispatcher.js
@@ -6,9 +6,13 @@ var conf = __dirname+'/conf.json';
 if(require('fs').existsSync(conf)){
   nconf.file({file: conf});
   var observe_folder = nconf.get('folder');
+  var opt = {
+    ignore: nconf.get('ignore') || [],
+    only: nconf.get('only') || []
+  };
   
   if(observe_folder){
-    app.run(observe_folder);
+    app.run(observe_folder,opt);
   }else{
     console.log([
     '\033[31m',
